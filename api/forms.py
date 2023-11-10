5b48eeb8b629e4d5eb291e5fea1b283698e11e2e
Samyak Jain <samyak0331@gmail.com>
2023-11-10 15:51:44 -0500

    added login/registratioin

---
+++ api/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, validators
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email()])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', [validators.DataRequired(), validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [validators.DataRequired()])
