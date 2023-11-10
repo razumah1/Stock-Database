5728733cf41cc361c440096593e04bc39386c2e4
Suharta <suharta098@gmail.com>
2023-11-10 18:24:27 -0500

    Updated forms.py
    Made improvements with validation

--- api/forms.py
+++ api/forms.py
@@ -1,15 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, validators
+from wtforms import StringField, PasswordField, SubmitField,validators
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email()])
+    username = StringField('Username', [validators.DataRequired(),validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.DataRequired(),validators.Length(min=6, max=35), validators.Email()])
     password = PasswordField('New Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
-    confirm = PasswordField('Repeat Password')
-
+    confirm = PasswordField('Repeat Password',validators=[validators.EqualTo('password'), validators.DataRequired()])
+    submit = SubmitField('Sign Up')
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.DataRequired(), validators.Length(min=4, max=25)])
     password = PasswordField('Password', [validators.DataRequired()])
+    submit = SubmitField(label='Sign in')
